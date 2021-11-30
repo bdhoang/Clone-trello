ec58885e059384bc207d80e39d6bb4a604c0ff7c
Bùi Duy Hoàng <17020760@vnu.edu.vn>
2021-11-30 19:05:20 +0700

    fix link http to https

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    url: 'http://api.unsplash.com/',
+    url: 'https://api.unsplash.com/',
     clientKey: 'QmjMoEikOzYRJVHjmNqVos5prdp9nsxa7rqWYDLllcQ',
 };
 export default config;
